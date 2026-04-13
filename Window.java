c0b8d2e4931f405c46f226b2e0281fd50f887fd0
Acutis24 <hlbuchanan54@gmail.com>
2026-04-13 09:21:12 -0400

    Button Imports

--- Window.java
+++ Window.java
@@ -1,12 +1,16 @@
 import javax.swing.JFrame;//the actual window
 import javax.swing.JLabel;//more features
 import javax.swing.ImageIcon;//to add images
+import javax.swing.JButton; //to add buttons
+import java.awt.event.ActionListener;//to add action to the buttons
+import java.awt.event.ActionEvent;//to add action to the buttons
 
 public class Window {
     public static void main(String[] args) {
         JFrame frame = new JFrame(); //creates a window
         frame.setTitle("Window"); //sets the title of the window
         frame.setSize(500, 500); //size of the window
+        frame.setLayout(null); //sets the layout to null so we can set the position of the components
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes the window when you click the x button
         frame.setVisible(true); //makes the window visible
     }
